79a5a0d24c6967e632cb4ebf733610deef02731f
ankh2054 <charles.holtzkampf@gmail.com>
2017-11-21 09:26:38 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,58 @@
 #!/bin/sh
 
 # Install Wiki-JS
+install_wikijs() {
+    
+	VERSION=$(curl -L -s -S https://beta.requarks.io/api/version/stable)
 
-curl -sSo- https://wiki.js.org/install.sh | bash
+	echo "[1/3] Fetching latest build..."
+	curl -L -s -S https://github.com/Requarks/wiki/releases/download/v$VERSION/wiki-js.tar.gz | tar xz -C .
 
-# Start WikiJS
-node wiki start
+	echo "[2/3] Fetching dependencies..."
+	curl -L -s -S https://github.com/Requarks/wiki/releases/download/v$VERSION/node_modules.tar.gz | tar xz -C .
+
+	echo "[3/3] Removing sampleconfig file..."
+	rm  config.sample.yml
+
+	# Create Wikijs user with no login or password
+	adduser -h /var/wiki -D -S  wikijs
+
+	# Correct permissions on /var/wiki
+	chown -R wikijs:nogroup /var/wiki
+
+}
+
+
+# Configure MongoDB
+configure_mongodb() {
+    # Create the MongoDB and assign permissions  
+	mkdir -p /data/db
+	chmod -R 0700 /data/db
+    chown -R mongodb:mongodb /data/db
+
+	# Start mongoDB
+	mongod --fork --logpath /var/log/mongod.log
+
+# Create mongoDB Database
+mongo <<EOF
+use wiki
+quit()
+EOF
+
+	# Stop the mongoDB service
+	mongod --dbpath /data/db --shutdown
+
+}
+
+# Do the stuff
+install_wikijs
+configure_mongodb
+
+
+# Supervisor
+# Start Supervisor 
+echo "Starting Supervisor"
+/usr/bin/supervisord -n -c /etc/supervisord.conf
+
+
+
